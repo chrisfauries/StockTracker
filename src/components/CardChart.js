6c393ee4283491223f16273d129ad7e9008f23df
StealthLSU <45839261+StealthLSU@users.noreply.github.com>
2019-03-16 17:55:22 -0500

    added chart layout

--- src/components/CardChart.js
+++ src/components/CardChart.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Bar, Line, Pie} from 'react-chartjs-2'
 
 class CardChart extends Component {
   state = {
-    stockData: []
+    stockData:{
+      labels: ['9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00'],
+      datasets:[
+        {
+          label:'Stock ',
+          data:[]
+        }
+      ]
+    }
   }
   
   componentWillMount(){
@@ -12,17 +21,28 @@ class CardChart extends Component {
       .then(res => {
        return (
         this.setState({
-          stockData: [...this.state.stockData, res.data]
+          stockData: [res.data]
         })
        )
       })
     }
 
   render() {
-    console.log(this.state.stockData);
+    const asdf = [123, 155]
+    console.log(this.state.stockData)
+    const post = this.state.stockData[0] ? (
+      <Line
+          data={asdf}
+          width={100}
+          height={50}
+          options={{}}
+        />
+    ): (
+      <div>Loading chart...</div>
+    )
     return (
       <div>
-        <p>This is a chart for: { this.props.symbol }</p>
+        { post }
       </div>
     )
   }
