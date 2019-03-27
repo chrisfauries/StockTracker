949dfe64ec2e56ca7ff482a932a0b8138641fcae
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-03-26 23:28:48 -0500

    minor css changes to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -47,7 +47,7 @@ class Cards extends Component {
       stocks.map(stock => {
 
         return (
-          <div className="card col s12 m6 l4 waves-effect waves-block waves-light z-depth-0" key={ shortid.generate() }>
+          <div className="card col s12 m6 l4 z-depth-0" key={ shortid.generate() }>
             <div className='card medium grey lighten-5 hoverable'>
               <div className='card-content black-text'>
                 <CardHeader stock={ stock } />
